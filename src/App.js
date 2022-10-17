42a2b9593b079d023277010efc84a94ae7012aa3
kakarlasrija <srijakakarla8@gmail.com>
2022-10-17 15:12:57 +0530

    CODE COMMIT

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import Sidebar from "./Components/Sidebar";
+import Feed from "./Components/Feed";
+import Rightbar from "./Components/Rightbar";
+import Navbar from "./Components/Navbar";
+import Add from "./Components/Add";
+
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
